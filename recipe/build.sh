9402ffbb7ace8bc9ff2f546870100a0f9e33ef3b
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-22 12:35:41 +0200

    all: enable OSX ARM64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,26 @@
 mkdir build
 cd build
 
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+  export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+fi
+
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_TESTING=OFF \
       -DPYTHON_EXECUTABLE=$PYTHON
-make
+
+make -j${CPU_COUNT}
 make install
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo $BUILD_PREFIX
+  echo $PREFIX
+  sed -i.back 's|'"$BUILD_PREFIX"'|'"$PREFIX"'|g' $PREFIX/lib/cmake/pycppad/pycppadTargets.cmake
+  rm $PREFIX/lib/cmake/pycppad/pycppadTargets.cmake.back
+fi
